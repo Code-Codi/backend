deeefe4ee231af33897de41e37cdc47dc1b13f76
mk-star <ssunn0812@gmail.com>
2025-05-17 14:08:36 +0900

    feat: BaseEntity 추가

--- src/main/java/com/codiapp/codi/CodiApplication.java
+++ src/main/java/com/codiapp/codi/CodiApplication.java
@@ -3,8 +3,10 @@ package com.codiapp.codi;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class CodiApplication implements CommandLineRunner {
     public static void main(String[] args) {
         SpringApplication.run(CodiApplication.class, args);
