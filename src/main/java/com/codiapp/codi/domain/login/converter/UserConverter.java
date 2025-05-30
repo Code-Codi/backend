82b8c8f45f9bfe39a775282d690eebf29f4908d1
kimSR0916 <143482628+kimSR0916@users.noreply.github.com>
2025-05-30 13:24:04 +0900

    REFACTOR: login, signup 오류 수정

--- src/main/java/com/codiapp/codi/domain/login/converter/UserConverter.java
+++ src/main/java/com/codiapp/codi/domain/login/converter/UserConverter.java
@@ -6,7 +6,7 @@ import com.codiapp.codi.domain.login.entity.User;
 
 public class UserConverter {
     public static LoginResponseDTO toLoginResponseDTO(User user) {
-        return new LoginResponseDTO(user.getEmail(), user.getUsername());
+        return new LoginResponseDTO(user.getId(), user.getEmail(), user.getUsername());
     }
 
     public static User toUser(SignupRequestDTO dto, String encodedPassword) {
