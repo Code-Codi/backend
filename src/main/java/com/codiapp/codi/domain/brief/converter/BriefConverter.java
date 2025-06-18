05f9ab036b8c4ebdd78dfd9b8f0f14b6e5e5514e
joamksh <joamksh@naver.com>
2025-06-18 10:43:05 +0900

    refactor: user에서 courser로 연결 변경

--- src/main/java/com/codiapp/codi/domain/brief/converter/BriefConverter.java
+++ src/main/java/com/codiapp/codi/domain/brief/converter/BriefConverter.java
@@ -6,7 +6,7 @@ import com.codiapp.codi.domain.brief.dto.response.BriefDetailResponseDTO;
 import com.codiapp.codi.domain.brief.dto.response.BriefResponseDTO;
 import com.codiapp.codi.domain.brief.entity.Brief;
 import com.codiapp.codi.domain.brief.entity.BriefDetail;
-import com.codiapp.codi.domain.user.entity.User;
+import com.codiapp.codi.domain.course.entity.Course;
 
 import java.time.LocalDateTime;
 import java.util.List;
@@ -30,12 +30,12 @@ public class BriefConverter {
     }
 
     //dto-> entity
-    public static Brief toTaskGuide(BriefCreateRequestDTO request, User user) {
+    public static Brief toTaskGuide(BriefCreateRequestDTO request, Course course) {
         Brief brief = Brief.builder()
                 .title(request.title())
                 .dueDate(request.dueDate())
                 .createdAt(LocalDateTime.now())
-                .user(user)
+                .course(course)
                 .build();
         return brief;
     }
--- src/main/java/com/codiapp/codi/domain/brief/entity/Brief.java
+++ src/main/java/com/codiapp/codi/domain/brief/entity/Brief.java
@@ -1,6 +1,7 @@
 package com.codiapp.codi.domain.brief.entity;
 
 import com.codiapp.codi.domain.brief.dto.request.BriefUpdateRequestDTO;
+import com.codiapp.codi.domain.course.entity.Course;
 import com.codiapp.codi.domain.user.entity.User;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
@@ -34,8 +35,8 @@ public class Brief {
     private Long id;
 
     @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name="user_id")
-    private User user;
+    @JoinColumn(name="course_id")
+    private Course course;
 
     private String title;
 
